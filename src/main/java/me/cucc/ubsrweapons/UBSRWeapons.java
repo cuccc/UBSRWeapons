5c70c4afb572be9f626ac2b1adcf44b7a102d6de
cuccc <gamesben753@gmail.com>
2024-04-14 17:33:59 +0200

    Initial commit

---
+++ src/main/java/me/cucc/ubsrweapons/UBSRWeapons.java
@@ -0,0 +1,17 @@
+package me.cucc.ubsrweapons;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class UBSRWeapons extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
